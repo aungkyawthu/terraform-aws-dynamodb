a7a5df9bbb2571b320fb980f9866c9aeb65a6822
Administrator <anil.gowda-h@hpe.com>
2024-08-29 11:27:18 +0530

    fix

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,14 +9,17 @@ module "dynamodb_table_01" {
   source = "../.."
   # source = "git::git@ssh.dev.azure.com:v3/petronasvsts/PETRONAS_AWS_IAC_Module/terraform-aws-dynamodb?ref=v5.0-latest"
 
-  create_table        = true
-  name                = "example-dynamodb-name"
-  hash_key            = "example-dynamodb-hash-key"
-  range_key           = "example-dynamodb-range-key"
-  billing_mode        = "PROVISIONED"
-  read_capacity       = 10
-  write_capacity      = 10
-  autoscaling_enabled = true
+  create_table        = var.create_table
+  autoscaling_enabled = var.autoscaling_enabled
+  name                = var.name
+  hash_key            = var.hash_key
+  range_key           = var.range_key
+  billing_mode        = var.billing_mode
+  read_capacity       = var.read_capacity
+  write_capacity      = var.write_capacity
+  stream_enabled   = var.stream_enabled
+  stream_view_type = var.stream_view_type
+  table_class      = var.table_class
 
   autoscaling_read = {
     scale_in_cooldown  = 10
@@ -26,22 +29,7 @@ module "dynamodb_table_01" {
     max_capacity       = 100
   }
 
-  autoscaling_write = {
-    scale_in_cooldown  = 10
-    scale_out_cooldown = 100
-    target_value       = 80
-    min_capacity       = 10
-    max_capacity       = 100
-  }
+  autoscaling_write = var.autoscaling_write
 
-  attributes = [
-    {
-      name = "example-dynamodb-hash-key"
-      type = "S"
-    },
-    {
-      name = "example-dynamodb-range-key"
-      type = "S"
-    }
-  ]
+  attributes = var.attributes
 }
