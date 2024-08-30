a475eb4518316c8f5ec5dc8e7764d97e3c82d743
Administrator <anil.gowda-h@hpe.com>
2024-08-30 13:22:27 +0530

    Update

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-# variable "access_key" {
-#   type        = string
-#   sensitive = true
-# }
+variable "access_key" {
+  type        = string
+  sensitive = true
+}
 
-# variable "secret_key" {
-#   type        = string
-#   sensitive = true
-# }
+variable "secret_key" {
+  type        = string
+  sensitive = true
+}
 
 variable "create_table" {
   description = "Controls if DynamoDB table and associated resources are created"
